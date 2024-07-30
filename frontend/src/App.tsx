3e3adf947a83b5f6aaa805b943f6f751c884d947
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2024-07-30 09:45:13 +0300

    learned dataflow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
-import Card from './Components/Card/Card';
+import CardList from './Components/CardList/CardList';
+import Counter from './Components/Counter/Counter';
+import Search from './Components/Search/Search';
+import Test from './Components/Test/Test';
 
 function App() {
+      const [search, setSearch] = useState<string>("");
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value);
+        console.log(e);
+    };
+
+    const onClick = (e: SyntheticEvent) => {
+        console.log(e);
+    };
   return (
     <div className="App">
-      <Card/>
+      <Counter/>
+      <Test/>
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      <CardList/>
     </div>
   );
 }
